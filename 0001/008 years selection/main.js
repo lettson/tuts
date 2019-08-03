9b5ef4f0dfa8f2fd65f09e28011dd8e4d1f3fdfc
lettson <mr.arturs.leja@gmail.com>
2019-08-04 01:18:39 +0300

    initial commit

--- 0001/008 years selection/main.js
+++
@@ -1,9 +0,0 @@
-  function years(start,end){
-    document.write("<select>")
-    for(var i = start; i<=end; i++){
-      document.write("<option>"+i+"</option>")
-    }
-    document.write("</select>")
-  }
-years(1,100)
-years(2000,2019)
