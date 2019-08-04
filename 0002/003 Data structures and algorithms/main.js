4595acafa3e9182ac6a20305aaaf9a2647af293b
lettson <mr.arturs.leja@gmail.com>
2019-08-04 21:55:12 +0300

    003 initial commit

---
+++ 0002/003 Data structures and algorithms/main.js
@@ -0,0 +1,21 @@
+//Stacks
+
+// functions: push, pop, 
+// peek - display top element
+// length 
+
+//palindrome - spelled the same forwards and backwards
+ 
+/*
+
+var letters = [];
+
+var word = "rscecar";
+
+var rward = "";
+
+
+
+
+
+/*   */
