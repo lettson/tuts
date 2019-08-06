d6e5677322613e706e2e8a6a15d88a3331fcd6e8
lettson <mr.arturs.leja@gmail.com>
2019-08-06 23:04:49 +0300

    update

--- 0002/003 Data structures and algorithms/main.js
+++ 0002/003 Data structures and algorithms/main.js
@@ -6,16 +6,41 @@
 
 //palindrome - spelled the same forwards and backwards
  
+
 /*
+//arrays have all the functions of a stack
+
+var rnd = ["x","x","x","x","x","x","x"];
 
 var letters = [];
 
-var word = "rscecar";
+var word = "racecar";
 
-var rward = "";
+var rword = "";
 
+for (var i=0; i < word.length; i++) {
+  letters.push(word[i]);
+}  
 
+console.log(letters);
 
+for (var i=0; i < word.length; i++) {
+  rword += rnd.pop();
+}
+console.log(rword);
+
+if (rword = word) {
+  console.log(word + " is a palindrome")
+} else {
+  console.log(word + " is NOT a palindrome")
+}
 
 
 /*   */
+
+var Stack = function() {
+  this.count = 0;
+  this.storage = {};
+}
+
+ 
